ff7ddb80a6a66e41c1bffbf2babe636ea56d0d45
Bienvenido Quintna <BienvenidoQuintana34@gmail.com>
2020-03-27 00:56:08 +0000

    update

--- NYC&Miami connenction.js
+++ NYC&Miami connenction.js
@@ -9,10 +9,11 @@ import { Jumbotron } from "./jumbotron";
  " https://media.istockphoto.com/photos/subway-train-arriving-to-platform-harlem-manhattan-nyc-usa-picture-id694691542",
  "https://s.abcnews.com/images/US/subway-1-gty-file-ml-190524_hpMain_4x3_992.jpg",
  "https://upload.wikimedia.org/wikipedia/commons/7/70/Miami_Metrorail_Hitachi_train_20190117.jpg",
- "https://upload.wikimedia.org/wikipedia/commons/7/70/Miami_Metrorail_Hitachi_train_20190117.jpg"
-];
-
-export function Home() {
+ "https://danphotographycool.files.wordpress.com/2013/01/dsc02866.jpg?w=848",
+ "https://i.pinimg.com/originals/bb/0c/90/bb0c9020794a7bc80c5dc476f1ee5da4.jpg",
+ "https://orenstransitpage.com/otpnycpics/r62int.jpg"
+ ]
+ export function Home() {
 	return (
 		<>
 			<Navbar />
